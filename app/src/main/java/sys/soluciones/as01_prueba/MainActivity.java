5fc6b2e10b44e9418e46fb54884bb3f5dfd65905
Gonzalo Neira <ggneiracc68@gmail.com>
2023-03-21 04:11:49 -0500

    v01 (master). Proyecto nuevo solo con msj de saludo...

---
+++ app/src/main/java/sys/soluciones/as01_prueba/MainActivity.java
@@ -0,0 +1,14 @@
+package sys.soluciones.as01_prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
